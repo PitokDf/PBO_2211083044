09d60d402f011c7a0245c78766a822fae394c0a0
PitokDf <pitokfauzi@gmail.com>
2023-03-16 14:34:37 +0700

    update

--- src/main/java/Pito16032023/Latihan1Buffer.java
+++ src/main/java/Pito16032023/Latihan1Buffer.java
@@ -3,35 +3,42 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Pito16032023;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import javax.swing.JOptionPane;
 
-public class Latihan1Buffer {
-public static void main(String[] args) {
-int number = 0;
-char letter = ' ';
-boolean result = false;
-String str = "";
-    try {
-        number = Integer.parseInt(JOptionPane.showInputDialog("Masukkan Number : "));
-        letter = JOptionPane.showInputDialog("Masukkan Letter : ").charAt(0);
-        result = Boolean.parseBoolean(JOptionPane.showInputDialog("Masukkan nilai result (true/false) : "));
-        str = JOptionPane.showInputDialog("Masukkan sebuah str : ");
+public class Latihan1Buffer{
+    public static void main(String[] args){
+        int number = 10;
+        char letter = 'a';
+        boolean result = true;
+        String str = "Hello";
+        
+        BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Anda memasukkan nilai:");
-        System.out.println("number = " + number);
-        System.out.println("letter = " + letter);
-        System.out.println("result = " + result);
-        System.out.println("str = " + str);
-    } catch (NumberFormatException ex) {
-        JOptionPane.showMessageDialog(null, "Terjadi kesalahan saat mengubah input menjadi bilangan bulat: " + ex.getMessage());
-    } catch (Exception ex) {
-        JOptionPane.showMessageDialog(null, "Terjadi kesalahan: " + ex.getMessage());
+        try {
+            System.out.print("Masukkan number : ");
+            number = Integer.parseInt(dataIn.readLine());
+            System.out.print("Masukkan letter : ");
+            letter = (char)dataIn.read();
+            dataIn.readLine();
+            System.out.print("Masukkan result (true/false): ");
+            result = Boolean.parseBoolean(dataIn.readLine());
+            System.out.print("Masukkan str : ");
+            str = dataIn.readLine();
+            
+            
+            System.out.println("Number : "+number);
+            System.out.println("Letter : "+letter);
+            System.out.println("Result : "+result);
+            System.out.println("Str : "+str);
+        }catch (IOException ex){
+            System.out.println("Terjadi kesalahan saat membaca input dari pengguna: " + ex.getMessage());
+        }
     }
-}
 }
 
 
--- src/main/java/Pito16032023/Latihan1JOPtionPane.java
+++ src/main/java/Pito16032023/Latihan1JOPtionPane.java
@@ -8,6 +8,19 @@ package Pito16032023;
  *
  * @author Pitok
  */
+import javax.swing.JOptionPane;
+
 public class Latihan1JOPtionPane {
-    
+public static void main(String[] args) {
+int number = 0;
+char letter = ' ';
+boolean result = false;
+String str = "";
+
+        number = Integer.parseInt(JOptionPane.showInputDialog("Masukkan Number : "));
+        letter = JOptionPane.showInputDialog("Masukkan Letter : ").charAt(0);
+        result = Boolean.parseBoolean(JOptionPane.showInputDialog("Masukkan nilai result (true/false) : "));
+        str = JOptionPane.showInputDialog("Masukkan sebuah str : ");
+        JOptionPane.showMessageDialog(null, "Number : " + number+"\nletter : "+letter+"\nresult : "+result+" \nString : "+str);
+}
 }
