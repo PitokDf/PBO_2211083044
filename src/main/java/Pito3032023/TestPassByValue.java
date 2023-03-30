f65e04c1f1c25b7377b73b67677cdd1919c37209
PitokDf <pitokfauzi@gmail.com>
2023-03-30 08:20:15 +0700

    create

---
+++ src/main/java/Pito3032023/TestPassByValue.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pito3032023;
+
+/**
+ *
+ * @author Pitok
+ */
+public class TestPassByValue {
+    public static void main(String[] args){
+        int i = 10;//mencetak nilai i
+        System.out.println(i);
+        
+        test(j);
+        System.out.println(i);
+    }
+    public static void test(int j){
+        j = 33;
+    }
+}
