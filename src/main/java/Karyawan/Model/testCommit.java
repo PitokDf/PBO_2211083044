661de5f7ce2e8c2345f0d626f21ff20c51afe1da
pitokdf <pitokfauzi@gmail.com>
2023-05-18 14:20:31 +0700

    commit

--- src/main/java/Karyawan/Model/testCommit.java
+++ src/main/java/Karyawan/Model/testCommit.java
@@ -3,5 +3,6 @@ package Karyawan.Model;
 public class testCommit {
     public static void main(String[] args) {
         System.out.println("hello dunia, apa kabar");
+        System.out.println("test ke-2 Commit");
     }
 }
