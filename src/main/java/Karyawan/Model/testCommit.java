f7b4b3fd0b61b55b4ea9587dfa4f50421546da87
PitokDf <108637369+PitokDf@users.noreply.github.com>
2023-05-18 13:30:46 +0700

    Update testCommit.java

--- src/main/java/Karyawan/Model/testCommit.java
+++ src/main/java/Karyawan/Model/testCommit.java
@@ -2,6 +2,6 @@ package Karyawan.Model;
 
 public class testCommit {
     public static void main(String[] args) {
-        System.out.println("hello dunia");
+        System.out.println("hello dunia, apa kabar");
     }
 }
